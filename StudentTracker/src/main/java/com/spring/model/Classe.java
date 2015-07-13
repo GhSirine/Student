003ae56197f67090c7511a9fbfa6e5c3af97d23f
GhSirine <sirineghezaiel1993@gmail.com>
2015-07-13 15:27:18 +0100

    a

--- StudentTracker/src/main/java/com/spring/model/Classe.java
+++ StudentTracker/src/main/java/com/spring/model/Classe.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+/*to be pushed*/
 @Entity
 @Table(name="CLASSE")
 public class Classe {
@@ -21,6 +21,27 @@ public class Classe {
 		super();
 		this.nomclasse = nomclasse;
 	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNomclasse() {
+		return nomclasse;
+	}
+
+
+	public void setNomclasse(String nomclasse) {
+		this.nomclasse = nomclasse;
+	}
+	
 	
 
 }
